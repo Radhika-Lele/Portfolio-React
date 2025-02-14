36de05cc6696eeee8e43ca2298d48857602565b2
Radhika-Lele <radhika.khaparde@gmail.com>
2025-02-14 15:40:08 -0500

    Complete Navbar & its responsiveness

--- src/components/Theme/Theme.jsx
+++ src/components/Theme/Theme.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import './Theme.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+
+const getLocalStorageTheme = () => {
+    let themes = 'light-theme';
+    if(localStorage.getItem('themes')){
+        themes = localStorage.getItem('themes');
+    }
+    return themes;
+}
 
 const Theme = () => {
+    const [theme, setTheme] = useState(getLocalStorageTheme());
+
+    const toggleTheme = () => {
+        if(theme === 'light-theme'){
+            setTheme('dark-theme')
+        }else{
+            setTheme('light-theme')
+        }
+    }
+    useEffect(() => {
+        document.documentElement.className = theme;
+        localStorage.setItem('themes', theme);
+    }, [theme])
     return(
-        <>
-        Theme
-        </>
+        <span className='Bg-toggler' onClick={toggleTheme}>
+            <FontAwesomeIcon icon={faMoon} />
+        
+        </span>
     )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import './Home.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import HeaderImage from '../../assets/header1.jpeg';
+import { Link } from 'react-router-dom';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
     return(
-        <>
-        Hi
-        </>
+        <header>
+         <div className='container container-lg header-container'>
+            <img src={HeaderImage} alt="Header Image" />
+            <div className='header-right'>
+                <h1>
+                    <span>Hi, I'm Radhika Lele</span> <br />
+                Web Developer
+                </h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus consectetur adipisci rem vero nemo nihil doloribus sapiente amet atque corrupti quibusdam, sequi accusamus quisquam esse incidunt! Veritatis similique debitis eaque?
+                   
+                </p>
+                <Link className='btn' to={'/about'}>
+                About Me
+                <span>
+                    <FontAwesomeIcon icon={faArrowRight} />
+                </span>
+                </Link>
+            </div>
+         </div>
+        </header>
     )
 }
 
